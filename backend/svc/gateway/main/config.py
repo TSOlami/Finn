8132cf23a70333c3171272c170b4a5f1f821022e
TSOlami <tijani.saheed@yahoo.com>
2023-12-29 00:02:15 +0100

    add neccessary configs for the app

--- backend/svc/gateway/main/config.py
+++ backend/svc/gateway/main/config.py
@@ -7,3 +7,5 @@ load_dotenv()
 class ApplicationConfig:
     # Server secret key
     SECRET_KEY = os.environ["SECRET_KEY"]
+    AUTH_SERVICE_URL = os.environ["AUTH_SERVICE_URL"]
+    SERVER_ENV = os.environ["SERVER_ENV"]
