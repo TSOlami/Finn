5e82d84d608ec27c9d606ffcba095835255d5b66
TSOlami <tijani.saheed@yahoo.com>
2023-12-30 19:41:41 +0100

    Add backend config settings

---
+++ backend/svc/api-docs/main/config.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+class ApplicationConfig:
+    # Server secret key
+    SECRET_KEY = os.environ["SECRET_KEY"]
+    AUTH_SERVICE_URL = os.environ["AUTH_SERVICE_URL"]
+    SERVER_ENV = os.environ["SERVER_ENV"]
