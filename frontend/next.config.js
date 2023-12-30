ad6e5b420e683256393f3fc2674ddbb3318bedb8
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-12-30 01:41:36 +0100

    Chore: Add unsplash domain to config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "source.unsplash.com",
+			},
+		],
+	},
+};
