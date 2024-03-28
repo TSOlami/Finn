35a4ad9545b151562251fb469732991ac5aa0a65
TSOlami <tijani.saheed@yahoo.com>
2024-03-28 16:41:31 +0100

    Add logo to Navbar component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,9 +1,17 @@
+import Image from "next/image";
 import Link from "next/link";
+
 
 const Navbar = () => {
 	return (	
-		<header className="bg-white shadow px-4 py-5 mx-auto flex justify-between items-center w-full">
-			<div>
+		<nav className="bg-white shadow px-4 py-5 mx-auto flex justify-between items-center w-full">
+			<div className="flex items-center gap-2">
+				<Image
+				src={"/assets/images/finn-logo.png"}
+				width={40}
+				height={40}
+				alt="Finn Logo"
+				/>
 				<h1 className="font-extrabold text-xl text-green-800 animate-fade-in-left">
 					Finn
 				</h1>
@@ -11,7 +19,7 @@ const Navbar = () => {
 
 			<div className="flex text-sm items-center gap-2">
 				<Link
-					href={""}
+					href={"/signin"}
 					className="flex text-green-700 hover:text-green-800 font-semibold transition duration-300 ease-in-out animate-fade-in-right"
 				>
 					Log in{" "}
@@ -32,12 +40,12 @@ const Navbar = () => {
 				</Link>
 				<Link
 					className="bg-green-700 text-white px-4 py-2 rounded hover:bg-green-800 font-semibold transition duration-300 ease-in-out animate-fade-in-right"
-					href={""}
+					href={"/signup"}
 				>
 					Sign Up
 				</Link>
 			</div>
-		</header>
+		</nav>
 	);
 };
 
