3ea208fe5b79d578870afe5560e4de9ae70fd92d
TSOlami <tijani.saheed@yahoo.com>
2024-03-28 11:02:11 +0100

    Update Navbar styling and font size

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 
 const Navbar = () => {
-	return (
-		<header className="bg-white shadow px-3 py-4 mx-auto flex justify-between items-center w-full">
+	return (	
+		<header className="bg-white shadow px-4 py-5 mx-auto flex justify-between items-center w-full">
 			<div>
-				<h1 className="font-extrabold text-2xl text-green-800 animate-fade-in-left">
+				<h1 className="font-extrabold text-xl text-green-800 animate-fade-in-left">
 					Finn
 				</h1>
 			</div>
 
-			<div className="flex items-center gap-2">
+			<div className="flex text-sm items-center gap-2">
 				<Link
 					href={""}
 					className="flex text-green-700 hover:text-green-800 font-semibold transition duration-300 ease-in-out animate-fade-in-right"
