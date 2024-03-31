f42cd829ee509f5420194e7d67d6c5174e07514d
TSOlami <tijani.saheed@yahoo.com>
2024-03-31 20:09:59 +0100

    Update server run configuration

--- backend/svc/gateway/server.py
+++ backend/svc/gateway/server.py
@@ -3,4 +3,4 @@ from main import create_server
 # Create server
 if __name__ == '__main__':
 	server, _, _, _ = create_server()
-	server.run(debug=True , host='0.0.0.0', port=8080)
+	server.run(host='0.0.0.0', port=8080)
