1b323b5847d4cc8dc7a4d42d5b3c53159b586470
TSOlami <tijani.saheed@yahoo.com>
2024-03-31 20:43:30 +0100

    Update health endpoint route

--- backend/svc/gateway/main/routes/__init__.py
+++ backend/svc/gateway/main/routes/__init__.py
@@ -3,7 +3,7 @@ from flask import Blueprint, jsonify
 main = Blueprint('main', __name__)
 
 
-@main.route('/api/v1/health', methods=['GET'])
+@main.route('/health', methods=['GET'])
 def health():
 		print("Recieved a health check request...")
 		return jsonify({'status': 'ok'})
