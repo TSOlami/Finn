3b8f6ddbcc3cd341d783c84519c866933c623898
TSOlami <tijani.saheed@yahoo.com>
2024-04-02 07:35:46 +0100

    Update ServiceCard component props

--- frontend/src/components/ServiceCard.tsx
+++ frontend/src/components/ServiceCard.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 interface Props {
 	title: string;
-	details: string;
+	description: string;
 	link?: string;
 }
 
-const ServiceCard = ({ title, details, link }: Props) => {
+const ServiceCard = ({ title, description, link }: Props) => {
 	return (
 		<Link
 			href={link? link : "/"}
-			className="bg-white p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out"
+			className="bg-white p-6 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300 ease-in-out"
 		>
 			<h3 className="text-lg font-semibold text-green-700">{title}</h3>
-			<p className="text-gray-600 mt-2">{details}</p>
+			<p className="text-gray-600 mt-2">{description}</p>
 		</Link>
 	);
 };
