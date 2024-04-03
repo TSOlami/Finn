de77b3eeab09879d2a2fd08e9d035a76f269f60a
TSOlami <tijani.saheed@yahoo.com>
2024-04-03 21:38:11 +0100

    Fix API URL in validateUser function

--- frontend/src/utils/validateUser.ts
+++ frontend/src/utils/validateUser.ts
@@ -3,10 +3,10 @@ import axios from 'axios';
 // Function to fetch user details from the gateway
 export async function validateUser(): Promise<any> {
     try {
-        const response = await axios.get('finn.com/api/v1/auth/user');
+        const response = await axios.get('http://finn.com/api/v1/auth/user');
         return response.data;
-    } catch (error) {
-        console.error('Error fetching user:', error);
+    } catch (error: any) {
+        console.error('Error fetching user:', error.response);
         throw error;
     }
 }
