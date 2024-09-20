64cf16d829cf813e8e75bfe17f34d793e0adf937
Tijani Saheed Olalekan <113607787+TSOlami@users.noreply.github.com>
2024-09-20 09:51:18 +0100

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1, user-scalable=0" />
       <body className={montserrat.className}>
         <StoreProvider>
           {children}
